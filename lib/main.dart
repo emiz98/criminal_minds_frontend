16841b5f4d7a11240e118c33b4372ca2660458ba
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-02-19 21:55:17 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_police_app/pages/root_app.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Criminal Minds',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: DefaultTextStyle(
+        style:
+            GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white)),
+        child: Container(
+          color: Colors.white,
+          child: const RootApp(),
+        ),
+      ),
+    );
+  }
+}
